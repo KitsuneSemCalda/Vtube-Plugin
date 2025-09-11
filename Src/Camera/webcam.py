91f0b9f4443a85281897971fbf304bc95d096999
KitsuneSemCalda <arthuramagalhaes1312@gmail.com>
2025-09-11 07:47:57 -0300

    refactor(camera): create a separated function to start the camera and add a cap props to fps

--- Src/Camera/webcam.py
+++ Src/Camera/webcam.py
@@ -4,26 +4,30 @@ from typing import Optional
 
 class Webcam:
     def __init__(
-        self, device_index: int = 0, width: int = 640, height: int = 800, fps: int = 60
+        self, device_index: int = 0, width: int = 640, height: int = 480, fps: int = 60
     ):
         self.device_index = device_index
         self.width = width
         self.height = height
+        self.fps = fps
         self.cap: Optional[cv2.VideoCapture] = None
 
+    def start(self):
+        """Initialize the webcam"""
         self.cap = cv2.VideoCapture(self.device_index)
         self.cap.set(cv2.CAP_PROP_FRAME_WIDTH, self.width)
         self.cap.set(cv2.CAP_PROP_FRAME_HEIGHT, self.height)
+        self.cap.set(cv2.CAP_PROP_FPS, self.fps)
         if not self.cap.isOpened():
             raise RuntimeError(f"Can't open Webcam: {self.device_index}")
 
     def get_frame(self):
         """Return the actual frame from webcam"""
         if self.cap is None:
-            raise RuntimeError("Webcam can't initialized")
+            raise RuntimeError("Webcam not initialized. Call start() first.")
         ret, frame = self.cap.read()
         if not ret:
-            raise RuntimeError("Can't open frame of webcam")
+            raise RuntimeError("Can't read frame from webcam")
         return frame
 
     def release(self):
@@ -31,3 +35,11 @@ class Webcam:
         if self.cap:
             self.cap.release()
             self.cap = None
+
+    # Context manager support
+    def __enter__(self):
+        self.start()
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.release()
--- Src/main.py
+++ Src/main.py
@@ -3,9 +3,18 @@ import cv2
 
 cam = Webcam()
 
-while True:
-    frame = cam.get_frame()
-    cv2.imshow("Webcam Test", frame)
-    if cv2.waitKey(1) & 0xFF == ord("q"):
-        break
-cv2.destroyAllWindows()
+
+def main():
+    cam.start()
+
+    while True:
+        frame = cam.get_frame()
+        cv2.imshow("Webcam 60FPS", frame)
+        if cv2.waitKey(1) & 0xFF == ord("q"):
+            break
+    cv2.destroyAllWindows()
+    pass
+
+
+if __name__ == "__main__":
+    main()
